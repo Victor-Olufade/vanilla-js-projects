b129ff0b91a35d8405e420463d0664f6476144bf
Victor-Olufade <olufadevictor97@gmail>
2023-03-22 20:21:49 +0100

    add modal and questions set up

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,29 @@
 //using selectors inside the element
 // traversing the dom
+
+// Method 1
+
+// const buttons = document.querySelectorAll('.question-btn')
+
+// buttons.forEach((button)=>{
+//     button.addEventListener('click', (e)=>{
+//         let currentBtn = e.currentTarget.parentElement.parentElement;
+//         currentBtn.classList.toggle('show-text')
+//     })
+// })
+
+// Method 2
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question)=>{
+    const button = question.querySelector('.question-btn');
+    button.addEventListener('click', ()=>{
+        questions.forEach((item)=>{
+                    item !== question ? item.classList.remove('show-text') : null;
+                })
+        question.classList.toggle('show-text')
+    })
+    
+})
+
