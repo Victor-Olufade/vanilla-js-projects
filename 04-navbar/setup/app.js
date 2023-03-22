9b06ec0f51c56b45fa7dbdb8ec3342a7afdc3086
Victor-Olufade <olufadevictor97@gmail>
2023-03-22 12:16:49 +0100

    add setup for navbar and sidebar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,16 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+let navToggle = document.querySelector('.nav-toggle')
+let navLinks = document.querySelector('.links')
+
+navToggle.addEventListener('click', ()=>{
+    // if(navLinks.classList.contains('show-links')){
+    //     navLinks.classList.remove('show-links')
+    // }else{
+    //     navLinks.classList.add('show-links')
+    // }
+    navLinks.classList.toggle('show-links')
+})
